09d2ee65fb5400ca2a58866c9e11e4f61c96ad8a
Z3bra <ratunke@gmail.com>
2025-06-09 13:29:05 -0700

    upload

---
+++ Bootkit/Macros.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Pch.h"
+
+/* Calculates absolute address from a relative offset using GetPtr() */
+#define G_PTR( x )	( ULONG_PTR )( GetPtr( ) - ( ( ULONG_PTR ) & GetPtr - ( ULONG_PTR ) x ) )
+
+/* Place function in a specific section */
+#define D_SEC( x )	__pragma( code_seg( push, ".text$" #x ) )
+
+/* Cast as a pointer with the specified typedef */
+#define D_API( x )	__typeof__( x )
+
+/* Cast as a pointer-wide variable */
+#define U_PTR( x )	( ULONG_PTR )( x )
+
+/* Cast as a generic pointer */
+#define C_PTR( x )	( PVOID )( x )
+
+/* Arch Specific Macros */
+#if defined( _WIN64 )
+	/* Get the end of code: x64 */
+	#define G_END( x )	U_PTR( GetPtr( ) + 11 )
+#else
+	/* Get the end of code: x86 */
+	#define G_END( x )	U_PTR( GetPtr( ) + 10 )
+#endif
